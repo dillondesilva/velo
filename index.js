926b4e20fbc242f8345161cc0200d736f59029fd
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-07-21 21:04:18 +1000

    Two warm up speed tests are conducted followed by the formal speed test. Multiple image downloads with averaged speeds for improved accuracy

--- index.js
+++ index.js
@@ -2,15 +2,7 @@
 const imgURL = "https://i.dailymail.co.uk/i/pix/2015/09/01/18/2BE1E88B00000578-3218613-image-m-5_1441127035222.jpg";
 const imgSize = 19091;
 
-function displayResults(startTime, endTime) {
-    var elapsedTime = (endTime - startTime) / 1000;
-    var bitsLoaded = imgSize * 8;
-    var speedBps = (bitsLoaded / elapsedTime).toFixed(2);
-    var speedKbps = (speedBps / 1024).toFixed(2);
-    var speedMbps = (speedKbps / 1024).toFixed(2);
-    
-    console.log(`${speedBps}Bps`, `${speedKbps}Kbps`, `${speedMbps}Mbps`);
-    
+function displayResults(speedMbps) {
     $("#result").text(`${speedMbps} Mbps`);
     $("#loading").fadeOut("fast", function () {
       $("#result").fadeIn("fast", function () {
@@ -19,8 +11,18 @@ function displayResults(startTime, endTime) {
     });
 }
 
-// Run speed test by downloading image
-function runSpeedTest() {
+function calcSpeed(startTime, endTime) {
+  var elapsedTime = (endTime - startTime) / 1000;
+  var bitsLoaded = imgSize * 8;
+  var speedBps = (bitsLoaded / elapsedTime).toFixed(2);
+  var speedKbps = (speedBps / 1024).toFixed(2);
+  var speedMbps = (speedKbps / 1024).toFixed(2);  
+
+  return speedMbps
+}
+
+// Initialise speed test download runs
+function initSpeedTest() {
   $("#start").fadeOut("fast", function () {
     $("#loading").fadeIn("fast", function () {
       console.log('first animation finished');
@@ -28,6 +30,35 @@ function runSpeedTest() {
   });
   console.log('Speed test running...');
 
+  // Executing warm up...
+  console.log('Executing warm up...')
+  executeTestIterations(0, 2, 0, (speedTotal) => {
+    var averageDLSpeed = speedTotal / 5;
+    console.log(`Warm up speed was ${averageDLSpeed}`);
+  })
+
+  // Now commencing formal speed test...
+  executeTestIterations(0, 5, 0, (speedTotal) => {
+    var averageDLSpeed = Math.round((speedTotal / 5) * 100) / 100;
+    displayResults(averageDLSpeed);  
+  })
+}
+
+// Recursive function which will keep track of the speed
+// total to average later
+function executeTestIterations(currentIter, numOfIter, speedTotal, callback) {
+  runSpeedTest((speed) => {
+    if (currentIter < numOfIter) {
+      executeTestIterations(currentIter + 1, numOfIter, speedTotal + speed, callback);
+    } else {
+      callback(speedTotal + speed);
+    }
+  });
+}
+
+// Runs a single speed test and returns back the internet
+// speed to be used in a callback func
+function runSpeedTest(callback) {
   // Init variables for timestamps
   var startTime, endTime;
 
@@ -37,7 +68,8 @@ function runSpeedTest() {
   // the speed will be calculated
   downloadImage.onload = () => {
     endTime = (new Date()).getTime();
-    displayResults(startTime, endTime);
+    var result = parseFloat(calcSpeed(startTime, endTime));
+    callback(result);
   }
 
   // Oops
